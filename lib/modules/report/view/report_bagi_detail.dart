e973fb5118855b6ca770f0a4de06d10bb0763852
mohamadsafik <safikmohamad14@gmail.com>
2024-05-17 23:04:24 +0700

    total customer

--- lib/modules/report/view/report_bagi_detail.dart
+++ lib/modules/report/view/report_bagi_detail.dart
@@ -364,6 +364,53 @@ class ReportBagiDretail extends StatelessWidget {
                         const SizedBox(height: 15),
                         const Divider(thickness: 2),
                         const SizedBox(height: 15),
+                        (state.invoice?.feeGarapin == null ||
+                                state.invoice?.feeGarapin == 0)
+                            ? SizedBox()
+                            : Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                    Container(
+                                        alignment: Alignment.center,
+                                        width: baseWidth / 7,
+                                        child: Text("CUST",
+                                            textAlign: TextAlign.center,
+                                            style: AppFont.medium(context))),
+                                    Container(
+                                      alignment: Alignment.center,
+                                      width: baseWidth / 7,
+                                      child: Text(
+                                        "Customer",
+                                        style: AppFont.medium(context),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                    Container(
+                                      alignment: Alignment.center,
+                                      width: baseWidth / 7,
+                                      child: Text(
+                                          state.invoice?.feeGarapin
+                                                  .toString() ??
+                                              "0",
+                                          textAlign: TextAlign.center,
+                                          style: AppFont.medium(context)),
+                                    ),
+                                    Container(
+                                      alignment: Alignment.center,
+                                      width: baseWidth / 7,
+                                      child: Text(
+                                          textAlign: TextAlign.center,
+                                          "-",
+                                          style: AppFont.medium(context)),
+                                    )
+                                  ]),
+                        const SizedBox(height: 15),
+                        (state.invoice?.feeGarapin == null ||
+                                state.invoice?.feeGarapin == 0)
+                            ? SizedBox()
+                            : const Divider(thickness: 2),
+                        const SizedBox(height: 15),
                         Align(
                           alignment: Alignment.centerRight,
                           child: Container(
@@ -411,16 +458,22 @@ class ReportBagiDretail extends StatelessWidget {
                                               style:
                                                   AppFont.mediumBold(context)),
                                           const SizedBox(width: 12),
+                                          // harus dibuka
                                           Text(
-                                              state.split?.split?.routes
-                                                      ?.map((e) => e.fee)
-                                                      .reduce(
-                                                          (value, element) =>
-                                                              value! + element!)
-                                                      .toString()
-                                                      .currencyDot(
-                                                          symbol: "Rp.") ??
-                                                  "",
+                                              (((state.split?.split?.routes
+                                                              ?.map(
+                                                                  (e) => e.fee)
+                                                              .reduce((value,
+                                                                      element) =>
+                                                                  value! +
+                                                                  element!)) ??
+                                                          0 +
+                                                              (state.invoice
+                                                                      ?.feeGarapin ??
+                                                                  0)) ??
+                                                      "")
+                                                  .toString()
+                                                  .currencyDot(symbol: "Rp."),
                                               textAlign: TextAlign.center,
                                               style:
                                                   AppFont.mediumBold(context)),
